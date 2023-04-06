3e3895bfe3d37b25df4413c4adf365d560d7061a
maulik-raja <rajamaulik16@gmail.com>
2023-04-06 11:46:23 +0530

    changes done

--- ecomm_project/settings.py
+++ ecomm_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from email_cred import _email_id, _email_password
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -158,5 +160,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "rajamaulik16@gmail.com"
-EMAIL_HOST_PASSWORD = "gdzbbgtsymnsygps"
+EMAIL_HOST_USER = _email_id
+EMAIL_HOST_PASSWORD = _email_password
